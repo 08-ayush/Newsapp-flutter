1907d62e89900d6fe798904c8b11ff54133aeb9c
08-ayush <aayushchauhan008@gmail.com>
2023-08-19 22:52:51 +0530

    first commit

---
+++ lib/models/ListItem.dart
@@ -0,0 +1,18 @@
+class ListItem {
+  String imgUrl;
+  String newsTitle;
+  String author;
+  String date;
+  String content;
+  String description;
+
+  ListItem(
+    this.imgUrl,
+    this.newsTitle,
+    this.author,
+    this.date,
+    this.content,
+    this.description,
+  );
+}
+
